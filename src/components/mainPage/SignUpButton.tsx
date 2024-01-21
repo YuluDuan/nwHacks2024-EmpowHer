27c444fd244a10aa700ddc79529369d8bd511df3
annaz29 <azz@DESKTOP-99IBU0N.localdomain>
2024-01-20 18:23:20 -0800

    Colours

---
+++ src/components/mainPage/SignUpButton.tsx
@@ -0,0 +1,9 @@
+function SignUpButton () {
+    return (
+        <button className="p-1 rounded-md bg-green text-black font-body font-bold">
+            SIGN UP
+        </button>
+    )
+}
+//className="rounded-md bg-green-dark text-black font-body font-bold"
+export default SignUpButton;
