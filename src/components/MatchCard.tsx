1a1449dc8d5ce4a3ef8ba0bab14713e277bcec5f
UBC Student <2860128966@qq.com>
2024-01-21 07:55:24 -0800

    fix the image

--- src/components/MatchCard.tsx
+++ src/components/MatchCard.tsx
@@ -10,7 +10,7 @@ const MatchCard = (props: {
 }) => {
   return (
     <div className="w-1/3 h-2/3 bg-green-dark rounded-3xl text-center flex flex-col items-center py-10 gap-8 px-4">
-      <Image src={props.imageURL} width={50} height={50} alt="Avatar" />
+      <img src={props.imageURL} width={50} height={50} alt="Avatar" />
 
       <p className="text-xl">
         {props.firstName} {props.lastName}
